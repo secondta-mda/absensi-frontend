2c1b59af1bdbe5c4ce74ce104da19a0622e150d8
secondta-mda <secondtaardiansyahw@gmail.com>
2025-08-22 10:27:11 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://api-absensimda.netlify.app'
+  baseURL: process.env.REACT_APP_API_URL || 'https://api-absensi-mda.netlify.app/api'
 });
 
 export default API;
